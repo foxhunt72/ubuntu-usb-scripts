528f53757cd714ff85be650c84311d93f9ed00ce
Richard de Vos <rdevos72@gmail.com>
2016-01-22 16:16:17 +0100

    change unmount fix for dev proc and sys

--- create_squash.sh
+++ create_squash.sh
@@ -37,9 +37,19 @@ do
 	        sudo sed -i "/${i}/d" ${CASPER_NEW_DIR}/filesystem.manifest-desktop
 done
 
-umount ${CHROOT_DIR}/proc
-umount ${CHROOT_DIR}/sys
-umount ${CHROOT_DIR}/dev
+for i in proc sys dev
+do
+  umount ${CHROOT_DIR}/${i}
+  umount -l ${CHROOT_DIR}/${i}
+
+  if ! df "${CHROOT_DIR}/${i}" | grep "${CHROOT_DIR}$"; then
+	echo "ERROR: $i is still mounted on ${CHROOT_DIR}/${i}"
+	exit 1
+  fi
+done
+
+# controleer of ze unmounted zijn
+
 
 
 # compress the chroot
