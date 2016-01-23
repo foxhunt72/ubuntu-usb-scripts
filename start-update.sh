1f0abc60534e1cd8ad3e38fecb06d388269085e4
Richard de Vos <rdevos72@gmail.com>
2016-01-23 23:00:09 +0100

    small enhancement

--- start-update.sh
+++ start-update.sh
@@ -6,3 +6,10 @@ mkdir "${CHROOT_DIR}"
 mkdir /mnt/ramdir
 
 mount -t overlayfs -o lowerdir=/rofs -o upperdir=/mnt/ramdir none "${CHROOT_DIR}"
+
+
+echo "new chroot created on $CHROOT_DIR"
+echo ""
+echo "use:"
+echo "./chroot.sh $CHROOT_DIR"
+echo ""
